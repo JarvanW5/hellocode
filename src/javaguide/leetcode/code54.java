85cb1d44551692257e9e19c7d7812609119e729e
wangjiawen <jarvanw1111@gmail.com>
2024-07-01 17:06:04 +0800

    leetcode No.54 螺旋矩阵

--- src/javaguide/leetcode/code54.java
+++ src/javaguide/leetcode/code54.java
@@ -16,28 +16,28 @@ public class code54 {
 
         List<Integer> ans = new ArrayList<>();
         int m = matrix.length, n = matrix[0].length;
-        int left = 0, right = n - 1, top = 0, bottom = m - 1;
-        while (left <= right && top <= bottom) {
+        int left = 0, right = n - 1, row = 0, column = m - 1;
+        while (left <= right && row <= column) {
             // 从左到右
             for (int i = left; i <= right; i++) {
-                ans.add(matrix[top][i]);
+                ans.add(matrix[row][i]);
             }
-            top++;
+            row++;
             // 从上到下
-            for (int i = top; i <= bottom; i++) {
+            for (int i = row; i <= column; i++) {
                 ans.add(matrix[i][right]);
             }
             right--;
             // 从右到左
-            if (top <= bottom) {
+            if (row <= column) {
                 for (int i = right; i >= left; i--) {
-                    ans.add(matrix[bottom][i]);
+                    ans.add(matrix[column][i]);
                 }
             }
-            bottom--;
+            column--;
             // 从下到上
             if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
+                for (int i = column; i >= row; i--) {
                     ans.add(matrix[i][left]);
                 }
             }
@@ -47,7 +47,7 @@ public class code54 {
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         List<Integer> ans = spiralOrder(matrix);
         System.out.println(ans);
     }
