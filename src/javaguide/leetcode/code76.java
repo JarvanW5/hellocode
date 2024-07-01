19a3fbcbe782f71cf7124b84da2841aa6d70dc8a
wangjiawen <jarvanw1111@gmail.com>
2024-07-01 10:58:13 +0800

    leetcode No.76 覆盖最小子串

---
+++ src/javaguide/leetcode/code76.java
@@ -0,0 +1,21 @@
+package javaguide.leetcode;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/1
+ * @Description: 覆盖最小子串
+ * @Requirements: 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+ */
+
+public class code76 {
+
+    public static String minWindow(String s, String t) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ADOBECODEBANC";
+        String s2 = "ABC";
+    }
+}
