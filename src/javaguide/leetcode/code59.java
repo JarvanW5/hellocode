a86dde9ef8f12c8981e1e680e58ac9c45a52d2e8
wangjiawen <jarvanw1111@gmail.com>
2024-07-01 14:15:47 +0800

    leetcode No.59 螺旋矩阵II

---
+++ src/javaguide/leetcode/code59.java
@@ -0,0 +1,63 @@
+package javaguide.leetcode;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/1
+ * @Description: 螺旋矩阵II
+ * @Requirements: 给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ */
+
+public class code59 {
+
+    public static int[][] generateMatrix(int n) {
+
+        int[][] nums = new int[n][n];
+        int startX = 0, startY = 0;  // 每一圈的起始点
+        int offset = 1;
+        int count = 1;  // 矩阵中需要填写的数字
+        int loop = 1; // 记录当前的圈数
+        int i, j; // j 代表列, i 代表行;
+
+        while (loop <= n / 2) {
+
+            // 顶部
+            // 左闭右开，所以判断循环结束时， j 不能等于 n - offset
+            for (j = startY; j < n - offset; j++) {
+                nums[startX][j] = count++;
+            }
+            // 右列
+            // 左闭右开，所以判断循环结束时， i 不能等于 n - offset
+            for (i = startX; i < n - offset; i++) {
+                nums[i][j] = count++;
+            }
+            // 底部
+            // 左闭右开，所以判断循环结束时， j != startY
+            for (j = n - offset; j > startY; j--) {
+                nums[i][j] = count++;
+            }
+            // 左列
+            // 左闭右开，所以判断循环结束时， i != startX
+            for (i = n - offset; i > startX; i--) {
+                nums[i][j] = count++;
+            }
+            startX++;
+            startY++;
+            offset++;
+            loop++;
+        }
+        if (n % 2 == 1) { // n 为奇数时，单独处理矩阵中心的值
+            nums[startX][startY] = count;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] ints = generateMatrix(n);
+        for (int i = 0; i < ints.length; i++) {
+            for (int i1 = 0; i1 < ints.length; i1++) {
+                System.out.println(ints[i][i1]);
+            }
+        }
+    }
+}
