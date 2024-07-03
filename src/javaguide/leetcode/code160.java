f05ea4af41d7f2f5d0807dd6a237819cf267a89a
wangjiawen <jarvanw1111@gmail.com>
2024-07-03 16:12:09 +0800

    leetcode No.160 链表相交

---
+++ src/javaguide/leetcode/code160.java
@@ -0,0 +1,87 @@
+package javaguide.leetcode;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/3
+ * @Description: 链表相交
+ * @Requirements: 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ */
+
+public class code160 {
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        while (curA != null) { // 求链表A的长度
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null) { // 求链表B的长度
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        // 让curA为最长链表的头，lenA为其长度
+        if (lenB > lenA) {
+            //1. swap (lenA, lenB);
+            int tmpLen = lenA;
+            lenA = lenB;
+            lenB = tmpLen;
+            //2. swap (curA, curB);
+            ListNode tmpNode = curA;
+            curA = curB;
+            curB = tmpNode;
+        }
+        // 求长度差
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+    // 用于打印链接列表的功能
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        // Create the linked list: 4 -> 1 -> 8 -> 4 -> 5
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = new ListNode(8);
+        headA.next.next.next = new ListNode(4);
+        headA.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original listA:");
+        printList(headA);
+
+        // Create the linked list: 5 -> 0 -> 1 -> 8 -> 4 -> 5
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(0);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = new ListNode(8);
+        headB.next.next.next.next = new ListNode(4);
+        headB.next.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original listB:");
+        printList(headB);
+        ListNode intersectionNode = getIntersectionNode(headA, headB);
+
+
+    }
+}
