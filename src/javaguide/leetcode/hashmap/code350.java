b02f911a2facef234ac5dccbc3207ce058372bea
wangjiawen <jarvanw1111@gmail.com>
2024-07-10 15:48:37 +0800

    leetcode No.350 两个数组的交集II

---
+++ src/javaguide/leetcode/hashmap/code350.java
@@ -0,0 +1,60 @@
+package javaguide.leetcode.hashmap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/10
+ * @Description: 两个数组的交集II
+ * @Requirements: 给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，
+ * <p>
+ * 应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2,2]
+ * 示例 2:
+ * <p>
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[4,9]
+ */
+
+public class code350 {
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {1, 2, 2, 3};
+        int[] intersect = intersect(nums1, nums2);
+        for (int i : intersect) {
+            System.out.println(i);
+        }
+    }
+}
