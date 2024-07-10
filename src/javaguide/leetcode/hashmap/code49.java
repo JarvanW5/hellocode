beec33dfe45d5d9d0d2191b91d20d09a598a2846
wangjiawen <jarvanw1111@gmail.com>
2024-07-10 10:57:30 +0800

    leetcode No.49 字母异位词分组

--- src/javaguide/leetcode/hashmap/code49.java
+++ src/javaguide/leetcode/hashmap/code49.java
@@ -1,5 +1,8 @@
 package javaguide.leetcode.hashmap;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -9,19 +12,65 @@ import java.util.List;
  * @Requirements: 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
  * <p>
  * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ * <p>
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ * <p>
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
  */
 
 public class code49 {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
 
-        
-        return null;
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+
+        return new ArrayList<List<String>>(map.values());
     }
 
 
     public static void main(String[] args) {
+
+        String word = "hello";
+        char[] charArray = word.toCharArray();
+
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+
+        Arrays.sort(charArray);
+
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+
+        String s = new String(charArray);
+        System.out.println(s);
+
+
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> lists = groupAnagrams(strs);
+
+        for (List<String> list : lists) {
+            System.out.println(list);
+        }
 
     }
 }
