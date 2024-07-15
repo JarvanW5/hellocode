195a5fde6052607e9667a8530581fad3fb81747a
wangjiawen <jarvanw1111@gmail.com>
2024-07-15 14:31:26 +0800

    leetcode No.18 四数之和

---
+++ src/javaguide/leetcode/hashmap/code18.java
@@ -0,0 +1,78 @@
+package javaguide.leetcode.hashmap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/15
+ * @Description: 四数之和
+ * @Requirements: 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。
+ * <p>
+ * 请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,2,2,2,2], target = 8
+ * 输出：[[2,2,2,2]]
+ */
+
+public class code18 {
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // nums[i] > target 直接返回, 剪枝操作
+            if (nums[i] > 0 && nums[i] > target) {
+                return result;
+            }
+
+            if (i > 0 && nums[i - 1] == nums[i]) {    // 对nums[i]去重
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (j > i + 1 && nums[j - 1] == nums[j]) {  // 对nums[j]去重
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (right > left) {
+                    // nums[k] + nums[i] + nums[left] + nums[right] > target int会溢出
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum > target) {
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        // 对nums[left]和nums[right]去重
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (right > left && nums[left] == nums[left + 1]) left++;
+
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> lists = fourSum(nums, target);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+    }
+}
