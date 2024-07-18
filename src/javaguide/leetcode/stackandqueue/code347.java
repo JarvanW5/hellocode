f5b787af77a818ebc24ca3f14461b67467ae565a
wangjiawen <jarvanw1111@gmail.com>
2024-07-18 10:45:49 +0800

    leetcode No.347 前K个高频元素

---
+++ src/javaguide/leetcode/stackandqueue/code347.java
@@ -0,0 +1,88 @@
+package javaguide.leetcode.stackandqueue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/18
+ * @Description: 前 K 个高频元素
+ * @Requirements: 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ */
+
+/*Comparator接口说明:
+ * 返回负数，形参中第一个参数排在前面；返回正数，形参中第二个参数排在前面
+ * 对于队列：排在前面意味着往队头靠
+ * 对于堆（使用PriorityQueue实现）：从队头到队尾按从小到大排就是最小堆（小顶堆），
+ *                                从队头到队尾按从大到小排就是最大堆（大顶堆）--->队头元素相当于堆的根节点
+ * */
+public class code347 {
+    //解法1：基于大顶堆实现
+    public static int[] topKFrequent1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>(); //key为数组元素值,val为对应出现次数
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        //在优先队列中存储二元组(num, cnt),cnt表示元素值num在数组中的出现次数
+        //出现次数按从队头到队尾的顺序是从大到小排,出现次数最多的在队头(相当于大顶堆)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair2[1] - pair1[1]);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {//大顶堆需要对所有元素进行排序
+            pq.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) { //依次从队头弹出k个,就是出现频率前k高的元素
+            ans[i] = pq.poll()[0];
+        }
+        return ans;
+    }
+
+    //解法2：基于小顶堆实现
+    public static int[] topKFrequent2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>(); //key为数组元素值,val为对应出现次数
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        //在优先队列中存储二元组(num, cnt),cnt表示元素值num在数组中的出现次数
+        //出现次数按从队头到队尾的顺序是从小到大排,出现次数最低的在队头(相当于小顶堆)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[1] - pair2[1]);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) { //小顶堆只需要维持k个元素有序
+            if (pq.size() < k) { //小顶堆元素个数小于k个时直接加
+                pq.add(new int[]{entry.getKey(), entry.getValue()});
+            } else {
+                if (entry.getValue() > pq.peek()[1]) { //当前元素出现次数大于小顶堆的根结点(这k个元素中出现次数最少的那个)
+                    pq.poll(); //弹出队头(小顶堆的根结点),即把堆里出现次数最少的那个删除,留下的就是出现次数多的了
+                    pq.add(new int[]{entry.getKey(), entry.getValue()});
+                }
+            }
+        }
+        int[] ans = new int[k];
+        for (int i = k - 1; i >= 0; i--) { //依次弹出小顶堆,先弹出的是堆的根,出现次数少,后面弹出的出现次数多
+            ans[i] = pq.poll()[0];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] ints = topKFrequent1(nums, k);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+
+        int[] ints1 = topKFrequent2(nums, k);
+        for (int i : ints1) {
+            System.out.println(i);
+        }
+
+    }
+}
