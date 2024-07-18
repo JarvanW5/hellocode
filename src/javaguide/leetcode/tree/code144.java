14a291ef1ae862a8b52b9742f31c65301ec1fb70
wangjiawen <jarvanw1111@gmail.com>
2024-07-18 14:12:50 +0800

    leetcode No.144 二叉树的前序遍历

---
+++ src/javaguide/leetcode/tree/code144.java
@@ -0,0 +1,54 @@
+package javaguide.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/18
+ * @Description: 二叉树的前序遍历
+ * @Requirements: 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ * 示例 2：
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[1]
+ */
+// 二叉树的前序遍历
+public class code144 {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        preorder(root, result);
+        return result;
+    }
+
+    // 前序遍历
+    public static void preorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        preorder(root.left, result);
+        preorder(root.right, result);
+    }
+
+    public static void main(String[] args) {
+        // 创建二叉树
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        // 执行前序遍历并打印结果
+        List<Integer> result = preorderTraversal(root);
+        System.out.println(result);
+    }
+}
