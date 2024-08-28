05dabb3f02c742bb5334bde38f11176ac8d898f0
wangjiawen <jarvanw1111@gmail.com>
2024-08-28 10:38:02 +0800

    commit 秋招练习leetcode hot100-1

--- src/leetcode_hot/code1.java
+++ src/leetcode_hot/code1.java
@@ -13,15 +13,13 @@ public class code1 {
     public static int[] twoSum(int[] nums, int target) {
 
         HashMap<Integer, Integer> hashMap = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(target - nums[i])) {
-                return new int[]{hashMap.get(target - nums[i]), i};
-            }
+            if (hashMap.containsKey(target - nums[i])) return new int[]{hashMap.get(target - nums[i]), i};
+
             hashMap.put(nums[i], i);
-
         }
-
-        return new int[]{};
+        return null;
     }
 
     public static void main(String[] args) {
