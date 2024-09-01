9c27249108db60a5190cb0b9e7e20f8bc552cf17
wangjiawen <jarvanw1111@gmail.com>
2024-09-01 14:14:20 +0800

    commit 秋招练习leetcode hot100-21

---
+++ src/leetcode_hot/code21new.java
@@ -0,0 +1,37 @@
+package leetcode_hot;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/9/1
+ * @Description:
+ * @Requirements:
+ */
+
+public class code21new {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 创建一个虚拟头节点，方便操作
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // 遍历两个链表
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        // 连接剩余的链表部分
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
