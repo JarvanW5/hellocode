76264bb42208ad8ee4243f8d2a7168fcea1ac51b
wangjiawen <jarvanw1111@gmail.com>
2024-09-02 10:04:40 +0800

    commit 秋招练习leetcode 创建Node

---
+++ src/leetcode_hot/Node.java
@@ -0,0 +1,21 @@
+package leetcode_hot;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/9/1
+ * @Description:
+ * @Requirements:
+ */
+
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+}
