e25cf18b46e6635ffe30eb80b12335ba31331145
wangjiawen <jarvanw1111@gmail.com>
2024-09-02 10:04:00 +0800

    commit 秋招练习leetcode hot100-138

---
+++ src/leetcode_hot/code138new.java
@@ -0,0 +1,19 @@
+package leetcode_hot;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/9/1
+ * @Description:
+ * @Requirements:
+ */
+
+public class code138new {
+
+    public static Node copyRandomList(Node head) {
+
+
+
+
+        return null;
+    }
+}
