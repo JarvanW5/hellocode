cc439810ed2610743b3ae4d6a172edf9aec43964
wangjiawen <jarvanw1111@gmail.com>
2024-09-02 10:04:06 +0800

    commit 秋招练习leetcode hot100-148

---
+++ src/leetcode_hot/code148.java
@@ -0,0 +1,30 @@
+package leetcode_hot;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/9/1
+ * @Description:
+ * @Requirements:
+ */
+
+public class code148 {
+    public static ListNode sortList(ListNode head) {
+
+        ListNode temp = new ListNode(0);
+
+        temp.next = head;
+//        while (temp != null) {
+//            if ()
+//        }
+
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+
+    }
+}
