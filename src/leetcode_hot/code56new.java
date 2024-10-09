81c98cded3de902caf6b67c0fb1e0b2bf962b067
wangjiawen <jarvanw1111@gmail.com>
2024-10-09 09:12:42 +0800

    commit leetcode56 合并区间

--- src/leetcode_hot/code56new.java
+++ src/leetcode_hot/code56new.java
@@ -15,18 +15,27 @@ public class code56new {
 
     public static int[][] merge(int[][] intervals) {
 
-        if (intervals.length == 0) return new int[0][];
+        // 1、检查输入是否为空
+        if(intervals == null || intervals.length == 0){
+            return new int[0][];
+        }
 
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        // 2、按照区间的起始位置排序
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
+        // 3、初始化一个 LinkedList 用于存储合并后的区间
         LinkedList<int[]> arrayList = new LinkedList<>();
+
+        // 4、遍历排序后的区间数组
         for (int[] interval : intervals) {
+            // 5、判断是否需要合并区间
             if (arrayList.isEmpty() || arrayList.getLast()[1] < interval[0]) {
                 arrayList.add(interval);
             } else {
                 arrayList.getLast()[1] = Math.max(arrayList.getLast()[1], interval[1]);
             }
         }
+        // 6、将 LinkedList 转换为二维数组并返回
         return arrayList.toArray(new int[arrayList.size()][]);
     }
 
