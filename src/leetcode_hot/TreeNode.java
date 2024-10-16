bebdd8557c6a5cac5b30c0b1e5474b29e0f0b5fc
wangjiawen <jarvanw1111@gmail.com>
2024-10-16 20:29:15 +0800

    commit leetcode 编写树节点TreeNode

--- src/leetcode_hot/TreeNode.java
+++ src/leetcode_hot/TreeNode.java
@@ -7,12 +7,21 @@ package leetcode_hot;
  * @Version:1.8
  * @Requirement:
  */
-class TreeNode {
+public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
 
-    public TreeNode(int val) {
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
         this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
