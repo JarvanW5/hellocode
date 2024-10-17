40ba852d3556cc0c6e66a216c6e4ad3f7a7f2f83
wangjiawen <jarvanw1111@gmail.com>
2024-10-17 10:09:12 +0800

    commit leetcode No.226 翻转二叉树

--- src/leetcode_hot/code226.java
+++ src/leetcode_hot/code226.java
@@ -13,14 +13,12 @@ public class code226 {
             return null;
         }
 
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
 
-        invertTree(root.left);
-        invertTree(root.right);
+        root.left = right;
+        root.right = left;
 
         return root;
-
     }
 }
