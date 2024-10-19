9bcde888c3a6f39a1b28d366a95061dbe9607889
wangjiawen <jarvanw1111@gmail.com>
2024-10-19 17:29:01 +0800

    commit leetcode No.226 二叉树的直径

--- src/leetcode_hot/code543.java
+++ src/leetcode_hot/code543.java
@@ -9,22 +9,28 @@ package leetcode_hot;
 
 public class code543 {
 
-    int ans;
+    private int maxDiameter = 0;
 
     public int diameterOfBinaryTree(TreeNode root) {
-        ans = 1;
         depth(root);
-        return ans - 1;
+        return maxDiameter;
     }
 
-    public int depth(TreeNode node) {
+
+    private int depth(TreeNode node) {
         if (node == null) {
-            return 0; // 访问到空节点了，返回0
+            return 0;
         }
-        int L = depth(node.left); // 左儿子为根的子树的深度
-        int R = depth(node.right); // 右儿子为根的子树的深度
-        ans = Math.max(ans, L + R + 1); // 计算d_node即L+R+1 并更新ans
-        return Math.max(L, R) + 1; // 返回该节点为根的子树的深度
+
+        int leftDepth = depth(node.left);
+        int rightDepth = depth(node.right);
+
+        int currentDiameter = leftDepth + rightDepth;
+
+        maxDiameter = Math.max(maxDiameter, currentDiameter);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+
     }
 
 }
