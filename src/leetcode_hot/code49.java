c5bf64d88f9ea40d38a7b1d1b3878abc3ab555a9
wangjiawen <jarvanw1111@gmail.com>
2025-02-19 10:43:54 +0800

    commit leetcode No.49学习

--- src/leetcode_hot/code49.java
+++ src/leetcode_hot/code49.java
@@ -12,8 +12,18 @@ import java.util.*;
 public class code49 {
     public static List<List<String>> groupAnagrams(String[] strs) {
 
+        HashMap<String, List<String>> hashmap = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String s = new String(charArray);
 
-        return null;
+            List<String> list = hashmap.getOrDefault(s, new ArrayList<>());
+            list.add(str);
+            hashmap.put(s, list);
+        }
+
+        return new ArrayList<>(hashmap.values());
     }
 
     public static void main(String[] args) {
